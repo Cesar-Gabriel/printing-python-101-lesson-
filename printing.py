5488ff7d3b6238153eeb377e30b592b7f5797a86
RED <121568700+Cesar-Gabriel@users.noreply.github.com>
2025-02-10 16:49:19 -0600

    Create printing.py

---
+++ printing.py
@@ -0,0 +1,22 @@
+# keep in mind that gray or green is that the command is NULL but put # as a note 
+
+# printing is the mos basic thing in python, to print (not in the printer, in the terminal) use print command, no UpPeRcAsE letters
+
+# use print ("Hello, world!") to print it (remember to have VScode), here, have a print already. i already put the print now on the "" put "Hello, world!"
+
+print ("")
+
+# an "," can separate text, and a "+" joins text
+
+print ("hello,",  "world!")
+print ("hello," + "world!")
+# leave with no "+" or "," to do less work, but remember to close with "" if you're not on VScode
+
+print ("hello, world!")
+
+# if says "syntax error" is beacuse you forgot a "()" or the "".)
+
+# to test run it on VScode and download the python extension, i recoment using a .py file, after that press that triangle, that means run code.
+
+# Thats the lesson of today 
+
